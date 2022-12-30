6b2ca3506399da9b347273b18dfca833d5ff1dca
davga1 <davvv.galstyan@gmail.com>
2022-12-30 12:54:12 +0400

    Happy New Year

---
+++ src/HWNY.java
@@ -0,0 +1,32 @@
+public class HWNY {
+    //HWNY --- HomeWork NewYear
+    public static void main(String[] args) {
+//թվերը տրված են պատահական,կարաս հանգիստ փոխես
+
+        int x;
+        int y;
+        x = 64;
+        y = 8;
+        double m = (double) x / y;
+        System.out.println(m);
+        System.out.println((int) m == m);
+
+        System.out.println("--------------------------------------");
+        System.out.println("PART 2");
+        System.out.println("--------------------------------------");
+        //թվերը տրված են պատահական,կարաս հանգիստ փոխես
+        double a = 16;
+        double b = 44;
+        double c = 85;
+        double d = 65;
+        double e = 45;
+        double f = 33;
+        double i = (a + b + c) / 3;
+        double j = (d + e + f) / 3;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println("k=" + (int) (i + j)  );
+
+        System.out.println("Happy գալիք  New Year Դավ ջան,քեզ ամենալավ բաները");
+    }
+}
