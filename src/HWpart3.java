9e22d6245b6b74649a102870c49895dbd39a9259
davga1 <davvv.galstyan@gmail.com>
2023-01-02 13:08:42 +0400

    Third Homework

---
+++ src/HWpart3.java
@@ -0,0 +1,14 @@
+
+public class HWpart3 {
+        public static void main(String[] args) {
+            double a = 52.125;
+            System.out.println("variant 1,a =" + (a - (int)a));
+
+            System.out.println("OR");
+
+            System.out.println("variant 2, a =" + (a % 1) );
+
+
+
+        }
+    }
